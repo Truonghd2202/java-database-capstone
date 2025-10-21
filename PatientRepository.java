ae72b206c31a405ee5597f8b7348b6d852e0339f
Truonghd2202 <dtruong2202@gmail.com>
2025-10-21 22:28:35 +0700

    Add files via upload

---
+++ PatientRepository.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.project.back_end.repo;
+
+/**
+ *
+ * @author TruongHD
+ */
+public interface PatientRepository {
+    
+}
